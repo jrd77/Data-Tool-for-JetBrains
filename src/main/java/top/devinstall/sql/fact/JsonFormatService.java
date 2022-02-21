15f45fe2ba263ef3a3522cc7f1f8c5b81fa3e2ef
jrd77 <tyxexeUFzjkh>
2022-02-21 17:27:22 +0800

    0.0.2 snapshot update

--- src/main/java/top/devinstall/sql/fact/JsonFormatService.java
+++ src/main/java/top/devinstall/sql/fact/JsonFormatService.java
@@ -1,6 +1,7 @@
 package top.devinstall.sql.fact;
 
 import com.cedarsoftware.util.io.JsonWriter;
+import top.devinstall.sql.util.StrUtil;
 import top.devinstall.sql.vo.SqlFormatReqVO;
 import top.devinstall.sql.vo.SqlFormatResVO;
 
@@ -9,6 +10,9 @@ public class JsonFormatService implements FormatService {
     @Override
     public SqlFormatResVO format(SqlFormatReqVO reqVO) {
 
+        if (!StrUtil.isJson(reqVO.getParamStr())) {
+            return new SqlFormatResVO(StrUtil.EMPTY);
+        }
         return new SqlFormatResVO(JsonWriter.formatJson(reqVO.getParamStr()));
     }
 }
